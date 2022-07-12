508ec1805259f5b84452f120c51c7c50b378dd84
Amir Jahan <74596803+amirjahan1@users.noreply.github.com>
2022-07-12 15:49:13 +0430

    Add files via upload

---
+++ function.js
@@ -0,0 +1,7 @@
+function saveCanvas() {
+    const imageLink = document.createElement("a"); // Important
+    const chartJs = document.getElementById("download-line-chart"); // you can change id # id = tag id
+    imageLink.download = "cleansing.jpeg"; // you can change name of file, can be dynamic
+    imageLink.href = chartJs.toDataURL("image/jpeg", 1); // you can change type of image, can be dynamic
+    imageLink.click();
+  }
